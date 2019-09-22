bfd0afae5d17d00a39d3c768759013b5d0879452
Dani Patiño Grisales <dani.grisales@hotmail.com>
2019-09-22 13:28:58 -0500

    primer login angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'apploguin';
+  public User: "daniela";
+  public email:"dani@hot.com";
+  public password: "1234";
+  public verBoton= true;
+  
+  
+
+  login(){
+    console.log('autenticando')
+  }
 }
